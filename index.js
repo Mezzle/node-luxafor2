b71129d9094f7bede94659c2e4ddf5c86f730b81
Jonathon Ball <jonathon.ball@gmail.com>
2018-09-30 22:27:40 -0400

    moved project into es6 class, implemented wave function as a test

--- index.js
+++ index.js
@@ -1,103 +1,102 @@
 var usb = require('usb');
-var Luxafor;
 const LUX_PID = 0xf372;
 const LUX_VID = 0x04d8;
 
-Luxafor = function () {
-	this.pid = LUX_PID;
-	this.vid = LUX_VID;
-	this.endpoint = undefined;
+class Luxafor {
+
+    constructor() {
+        this.pid = LUX_PID;
+    	this.vid = LUX_VID;
+    	this.endpoint = undefined;
+    }
+
+    init(cb) {
+    	var device = undefined,
+    	iface = undefined;
+
+    	var device = usb.findByIds(this.vid, this.pid);
+    	device.open();
+
+    	iface = device.interface(0);
+
+    	if (iface.isKernelDriverActive()) {
+    		iface.detachKernelDriver();
+    	}
+
+    	iface.claim();
+
+    	this.endpoint = iface.endpoint(1);
+
+    	//Dummy data
+    	var buff = new Buffer([0, 0]);
+    	this.endpoint.transfer(buff, function () {
+    		if (cb) {
+    			cb();
+    		}
+    	});
+    };
+
+    strobeColor(r, g, b, cb) {
+    	var buff = new Buffer(8);
+
+    	//Strobe
+    	buff.writeUInt8(3, 0);
+    	//"Both Sides"
+    	buff.writeUInt8(255, 1);
+
+    	buff.writeUInt8(r, 2);
+    	buff.writeUInt8(g, 3);
+    	buff.writeUInt8(b, 4);
+
+    	//"t" 10. Time?
+    	buff.writeUInt8(10, 5);
+
+    	//"d" ?
+    	buff.writeUInt8(0, 6);
+
+    	//"Re" 3. Repeat?
+    	buff.writeUInt8(3, 7);
+
+    	this.endpoint.transfer(buff, function () {
+    		if (cb) {
+    			cb();
+    		}
+    	});
+    };
+
+    setColor(r, g, b, cb) {
+    	var buff = new Buffer(5);
+
+    	//Jump
+    	buff.writeUInt8(1, 0);
+    	//"Both Sides"
+    	buff.writeUInt8(255, 1);
+
+    	buff.writeUInt8(r, 2);
+    	buff.writeUInt8(g, 3);
+    	buff.writeUInt8(b, 4);
+
+    	this.endpoint.transfer(buff, function () {
+    		if (cb) {
+    			cb();
+    		}
+    	});
+    };
+
+    wave(type, r, g, b, repeat, speed) {
+        var buff = new Buffer(8);
+        buff.writeUInt8(4, 0);
+        buff.writeUInt8(1, 1);
+        buff.writeUInt8(255, 2);
+        buff.writeUInt8(0, 3);
+        buff.writeUInt8(0, 4);
+        buff.writeUInt8(0, 5);
+        buff.writeUInt8(10, 6);
+        buff.writeUInt8(10, 7);
+        this.endpoint.transfer(buff);
+    }
+
 }
-
-Luxafor.prototype.init = function (cb) {
-	var device = undefined,
-	interface = undefined;
-
-	device = usb.findByIds(this.vid, this.pid);
-	device.open();
-
-	interface = device.interface(0);
-
-	if (interface.isKernelDriverActive()) {
-		interface.detachKernelDriver();
-	}
-
-	interface.claim();
-
-	this.endpoint = interface.endpoint(1);
-
-	//Dummy data
-	var buff = new Buffer([0, 0]);
-	this.endpoint.transfer(buff, function () {
-		if (cb) {
-			cb();
-		}
-	});
-};
-
-/*
-Luxafor.prototype.setLuxaforColor = function (color, cb) {
-	var buff =  new Buffer(2);
-
-	//Padding
-	buff.writeUInt8(0, 0);
-
-	buff.writeUInt8(color, 1);
-
-	this.endpoint.transfer(buff, function () {
-		if (cb) {
-			cb();
-		}
-	});
-};
-*/
-
-Luxafor.prototype.flashColor = function (r, g, b, cb) {
-	var buff = new Buffer(8);
-
-	//Strobe
-	buff.writeUInt8(3, 0);
-	//"Both Sides"
-	buff.writeUInt8(255, 1);
-
-	buff.writeUInt8(r, 2);
-	buff.writeUInt8(g, 3);
-	buff.writeUInt8(b, 4);
-
-	//"t" 10. Time?
-	buff.writeUInt8(10, 5);
-
-	//"d" ?
-	buff.writeUInt8(0, 6);
-
-	//"Re" 3. Repeat?
-	buff.writeUInt8(3, 7);
-
-	this.endpoint.transfer(buff, function () {
-		if (cb) {
-			cb();
-		}
-	});
-};
-
-Luxafor.prototype.setColor = function  (r, g, b, cb) {
-	var buff = new Buffer(5);
-
-	//Jump
-	buff.writeUInt8(1, 0);
-	//"Both Sides"
-	buff.writeUInt8(255, 1);
-
-	buff.writeUInt8(r, 2);
-	buff.writeUInt8(g, 3);
-	buff.writeUInt8(b, 4);
-
-	this.endpoint.transfer(buff, function () {
-		if (cb) {
-			cb();
-		}
-	});
-};
 
 module.exports = function () {
 	return new Luxafor();
