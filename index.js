abbd88237623d3b8cd93372ac96fdb6b7eca1681
Jon Ball <jonathon.ball@gmail.com>
2018-10-01 12:45:36 -0400

    updated app

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Luxafor {
         this.API = API;
     }
 
-    init(cb) {
+    init(cb = undefined) {
     	var device = undefined,
     	iface = undefined;
 
@@ -26,81 +26,9 @@ class Luxafor {
 
     	this.endpoint = iface.endpoint(1);
 
-    	//Dummy data
-    	var buff = new Buffer([0, 0]);
-    	this.endpoint.transfer(buff, function () {
-    		if (cb) {
-    			cb();
-    		}
-    	});
+        this.buffer = new Buffer([0, 0]);
+    	this.flushBuffer(cb);
     };
-
-    strobeColor(r, g, b, cb) {
-    	var buff = new Buffer(8);
-
-    	//Strobe
-    	buff.writeUInt8(3, 0);
-    	//"Both Sides"
-    	buff.writeUInt8(255, 1);
-
-    	buff.writeUInt8(r, 2);
-    	buff.writeUInt8(g, 3);
-    	buff.writeUInt8(b, 4);
-
-    	//"t" 10. Time?
-    	buff.writeUInt8(10, 5);
-
-    	//"d" ?
-    	buff.writeUInt8(0, 6);
-
-    	//"Re" 3. Repeat?
-    	buff.writeUInt8(3, 7);
-
-    	this.endpoint.transfer(buff, function () {
-    		if (cb) {
-    			cb();
-    		}
-    	});
-    };
-
-    setColor(r, g, b, cb = undefined) {
-    	var buff = new Buffer(5);
-
-    	//Jump
-    	buff.writeUInt8(1, 0);
-    	//"Both Sides"
-    	buff.writeUInt8(255, 1);
-
-    	buff.writeUInt8(r, 2);
-    	buff.writeUInt8(g, 3);
-    	buff.writeUInt8(b, 4);
-
-    	this.endpoint.transfer(buff, function () {
-    		if (cb) {
-    			cb();
-    		}
-    	});
-    };
-
-    /**
-     * @param Number type 0-5 the type of wave
-     * @param Number red 0-255 red value
-     * @param Number green 0-255 green value
-     * @param Number blue 0-255 blue value
-     * @param Number repeat 0-255, 0=forever, 1-255 # of iterations
-     * @param Number speed 0-255 0 = fast, 255 = slow
-     * @param Function a callback to execute
-     */
-    wave(type, r, g, b, repeat, speed, cb = undefined) {
-        this.newBuffer(8);
-        this.command(API.COMMAND.WAVE);
-        this.buffer.writeUInt8(type, API.BYTE.WAVE_TYPE);
-        this.rgb(r, g, b);
-        this.buffer.writeUInt8(0, API.BYTE.WAVE_PADDING);
-        this.buffer.writeUInt8(repeat, API.BYTE.WAVE_REPEAT);
-        this.buffer.writeUInt8(speed, API.BYTE.WAVE_SPEED);
-        this.flushBuffer(cb);
-    }
 
     newBuffer(size) {
         this.buffer = null;
@@ -115,14 +43,58 @@ class Luxafor {
         });
     }
 
-    command(command) {
-        this.buffer.writeUInt8(command, API.BYTE.COMMAND);
-    }
-
-    rgb(red, green, blue) {
+    strobeColor(red, green, blue, cb = undefined) {
+    	var buff = new Buffer(8);
+        this.newBuffer(8);
+    	this.buffer.writeUInt8(API.COMMAND.STROBE, API.BYTE.COMMAND);
+    	this.buffer.writeUInt8(API.LED.ALL, API.BYTE.LED);
         this.buffer.writeUInt8(red, API.BYTE.RED);
         this.buffer.writeUInt8(green, API.BYTE.GREEN);
         this.buffer.writeUInt8(blue, API.BYTE.BLUE);
+    	this.buffer.writeUInt8(10, API.BYTE.TIME);
+    	this.buffer.writeUInt8(0, API.BYTE.STROBE_PADDING);
+    	this.buffer.writeUInt8(3, API.BYTE.STROBE_REPEAT);
+        this.flushBuffer(cb);
+    };
+
+    setColor(red, green, blue, cb = undefined) {
+        this.newBuffer(5);
+    	this.buffer.writeUInt8(API.COMMAND.SET_COLOR_FADE, API.BYTE.COMMAND);
+    	this.buffer.writeUInt8(API.LED.ALL, API.BYTE.LED);
+        this.buffer.writeUInt8(red, API.BYTE.RED);
+        this.buffer.writeUInt8(green, API.BYTE.GREEN);
+        this.buffer.writeUInt8(blue, API.BYTE.BLUE);
+        this.flushBuffer(cb);
+    };
+
+    /**
+     * @param Number type 0-5 the type of wave
+     * @param Number red 0-255 red value
+     * @param Number green 0-255 green value
+     * @param Number blue 0-255 blue value
+     * @param Number repeat 0-255, 0=forever, 1-255 # of iterations
+     * @param Number speed 0-255 0 = fast, 255 = slow
+     * @param Function a callback to execute
+     */
+    wave(type, r, g, b, repeat, speed, cb = undefined) {
+        this.newBuffer(8);
+        this.buffer.writeUInt8(API.COMMAND.WAVE, API.BYTE.COMMAND);
+        this.buffer.writeUInt8(type, API.BYTE.WAVE_TYPE);
+        this.buffer.writeUInt8(red, API.BYTE.RED);
+        this.buffer.writeUInt8(green, API.BYTE.GREEN);
+        this.buffer.writeUInt8(blue, API.BYTE.BLUE);
+        this.buffer.writeUInt8(0, API.BYTE.WAVE_PADDING);
+        this.buffer.writeUInt8(repeat, API.BYTE.WAVE_REPEAT);
+        this.buffer.writeUInt8(speed, API.BYTE.WAVE_SPEED);
+        this.flushBuffer(cb);
+    }
+
+    pattern(pattern, repeat) {
+        this.newBuffer(3);
+        this.buffer.writeUInt8(API.COMMAND.PATTERN, API.BYTE.COMMAND);
+        this.buffer.writeUInt8(pattern, API.BYTE.PATTERN_ID);
+        this.buffer.writeUInt8(repeat, API.BYTE.PATTERN_REPEAT);
+        this.flushBuffer();
     }
 
 }
