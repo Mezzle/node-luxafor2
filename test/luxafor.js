18e43d3a57e64fb5ecfad474463de7637beb7dbd
Jonathon Ball <jonathon.ball@gmail.com>
2018-10-14 00:14:40 -0400

    added more tests

--- test/luxafor.js
+++ test/luxafor.js
@@ -16,6 +16,12 @@ describe('Luxafor', function() {
     describe('#getApiValue', function() {
         it('should be able to fetch API values', function() {
             assert.strictEqual(Luxafor.getApiValue('color', 'blue'), API.COLOR.BLUE);
+            assert.strictEqual(Luxafor.getApiValue('WAVE_TYPE', 'unknown_5'), API.WAVE_TYPE.UNKNOWN_5);
+            assert.strictEqual(Luxafor.getApiValue('byte', 'TIME'), API.BYTE.TIME);
+            assert.strictEqual(Luxafor.getApiValue('wave_type', 5), API.WAVE_TYPE.UNKNOWN_5);
+        });
+        it('should return null if there is nothing to find', function() {
+            assert.strictEqual(Luxafor.getApiValue('wave_type', 'disco'), null);
         });
     });
 });
